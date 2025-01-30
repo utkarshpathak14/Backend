c5ae72d37ba3cd181d5f19fd3e6992c4caeb4336
Utkarsh Pathak <utkarshpathakji285@gmail.com>
2025-01-30 11:36:10 +0530

    Project completed

---
+++ Desktop/Backend/Backend/src/index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { app,server } from "./lib/socket.js";
+
+
+
+dotenv.config();
+
+// Middleware
+app.use(cors({
+    origin: "http://localhost:5173", // Allow your frontend
+    credentials: true,              // Allow cookies
+}));
+
+app.use(express.json());
+app.use(cookieParser());
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+
+  
+
+const PORT = process.env.PORT;
+
+
+
+server.listen(PORT, () => {
+    console.log("Server started on port: " + PORT);
+    connectDB(); 
+});
